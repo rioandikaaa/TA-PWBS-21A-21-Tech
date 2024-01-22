50dc626c1a141d34fb273719f99d0a636f73af88
zahraksangha <zahra_kharisma_sangha@teknokrat.ac.id>
2024-01-22 18:41:30 +0700

    zahraks: UPLOAD ENDPOINT UNTUK MENAMPILKAN DETAIL DATA

--- BACKEND/server/routes/web.php
+++ BACKEND/server/routes/web.php
@@ -8,3 +8,9 @@ Route::get('/data_anak', [KiaController::class, 'index']);
 
 // Endpoint untuk mencari data anak berdasarkan keyword
 Route::get('/data_anak/search/{keyword}', [KiaController::class, 'search']);
+
+// Endpoint untuk menampilkan detail data anak berdasarkan nomer_antri
+Route::get('/data_anak/{nomer_antri}', [KiaController::class, 'show']);
+Route::get('/data_anak', [KiaController::class, 'index']);
+Route::get('/data_anak/search/{keyword}', [KiaController::class, 'search']);
+Route::get('/data_anak/show/{nomer_antri}', [KiaController::class, 'show']);
