b9112e968d57336511a80f354c852b6a5dc6a98c
zahraksangha <zahra_kharisma_sangha@teknokrat.ac.id>
2024-01-22 18:32:16 +0700

    zahraks: UPLOAD  ENDPOINT UNTUK MENCARI DATA ANAK

--- BACKEND/server/routes/web.php
+++ BACKEND/server/routes/web.php
@@ -5,3 +5,6 @@ use App\Http\Controllers\KiaController;
 
 // Endpoint untuk menampilkan semua data anak
 Route::get('/data_anak', [KiaController::class, 'index']);
+
+// Endpoint untuk mencari data anak berdasarkan keyword
+Route::get('/data_anak/search/{keyword}', [KiaController::class, 'search']);
